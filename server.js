f1b184c84be6b77f4653bc5b0e8b4452ac8687c4
Manogna <manognasamayam@gmail.com>
2025-08-06 21:21:12 +0530

    removed dotenv

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mysql = require("mysql2");
 const cors = require("cors");
 const path = require("path");
 const fs = require("fs");
-require("dotenv").config(); // Load .env
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
