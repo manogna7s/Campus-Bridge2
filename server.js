d3736d84bf2865147a4abec6bc468a15e4a811f1
Manogna <manognasamayam@gmail.com>
2025-08-08 22:08:57 +0530

    commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static files from public folder
+// Serve static fiiles from public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve uploaded PDFs
